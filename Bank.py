5b722d10472034f6c7fda1c52f763a025cf1088f
Rizwan Bashir <rizwan.bashir@student.uts.edu.au>
2024-03-11 18:47:21 +1100

    first skeleton of lab 4 primary exercise

---
+++ Bank.py
@@ -0,0 +1,25 @@
+class Account:
+    def __init__(self):
+        self.balance = 0.0
+        self.type = ""
+
+
+class Bank:
+
+
+
+
+class Customer:
+    def __init__(self):
+        self.name = ""
+        self.accounts = []
+
+
+class Manager:
+    def __init__(self):
+        self.name = ""
+
+
+if __name__ == "__main__":
+    bank = Bank()
+    bank.main()
